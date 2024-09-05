7d4c43bd1324a6cb63e29299a45f2871a6cdadf0
ZeqeB <85196913+ezebellino@users.noreply.github.com>
2024-09-05 00:40:29 +0000

    modificando el archivo app.js

--- src/js/app.js
+++ src/js/app.js
@@ -23,23 +23,24 @@ import "../style/index.css";
     }
  */
 function render(variables = {}) {
-  console.log("These are the current variables: ", variables); // print on the console
-  // here we ask the logical questions to make decisions on how to build the html
-  // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
+  console.log("These are the current variables: ", variables); 
+  
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  const name = variables.name == null ? "Nombre" : variables.name;
+  const lastName = variables.lastName == null ? "Apellido" : variables.lastName;
+  const role = variables.role == null ? "Tu rol" : variables.role;
+  const country = variables.country == null ? "País" : variables.country;
+  const city = variables.city == null ? "Ciudad" : variables.city;
 
-  let fullname = " ";
-  if (variables.name) fullname += variables.name;
-  if (variables.lastName) fullname += variables.lastName;
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>${variables.name} ${variables.lastName}</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
+          <h1>${name} ${lastName}</h1>
+          <h2>${role}</h2>
+          <h3>${city}, ${country}</h3>
+          <ul class=${variables.socialMediaPosition}>
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
@@ -73,12 +74,11 @@ window.onload = function() {
     country: null,
     city: null
   };
-  render(window.variables); // render the card for the first time
+  render(window.variables); 
 
   document.querySelectorAll(".picker").forEach(function(elm) {
     elm.addEventListener("change", function(e) {
-      // <- add a listener to every input
-      const attribute = e.target.getAttribute("for"); // when any input changes, collect the value
+      const attribute = e.target.getAttribute("for"); 
       let values = {};
       values[attribute] =
         this.value == "" || this.value == "null"
@@ -88,7 +88,7 @@ window.onload = function() {
           : this.value == "false"
           ? false
           : this.value;
-      render(Object.assign(window.variables, values)); // render again the card with new values
+      render(Object.assign(window.variables, values)); 
     });
   });
 };
